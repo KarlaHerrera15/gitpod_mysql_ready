7c13f916f57e2aa1bae0eebefcda28d31c2966fd
KarlaHerrera15 <162621653+KarlaHerrera15@users.noreply.github.com>
2025-09-16 09:19:04 +0000

    Creare un workspace gitpod con mysql

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
   host="localhost",
-  user="root",
-  password=""
+  user="pythonuser",
+  password="password123"
 )
 
 mycursor = mydb.cursor()
